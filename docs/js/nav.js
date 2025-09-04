0c5a924edb1cf738b1b9d214cfff6899ff51fa09
Autumn Valenta <autumn@discoverywritten.com>
2025-09-03 19:11:28 -0600

    Update nav sensitivity

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -3,33 +3,33 @@
     if (!!document.querySelector('#main')) {
       return;
     }
-    const headers = document
-      .querySelectorAll('h1, h2, h3, h4')
-      ;
+    const headers = document.querySelectorAll('h1, h2, h3, h4');
 
     for (const h of headers) {
       const top = parseInt(getComputedStyle(h).top, 0) || 0;
       const sentinel = document.createElement('span');
-      sentinel.style.cssText = 'display:block; height:1px; margin-top:-1px;';
+      sentinel.className = `sticky-sentinel ${h.tagName.toLowerCase()}`;
       h.parentNode.insertBefore(sentinel, h);
 
       new IntersectionObserver(([entry]) => {
         const r = h.getBoundingClientRect();
         h.classList.toggle('is-sticky', (
-          r.top < window.innerHeight
+          r.top < window.innerHeight // don't analyze lower headers
           && !entry.isIntersecting
         ));
       }, {
-        root: null, rootMargin: `-${top}px 0px 0px 0px`, threshold: 0,
+        // Keep top-margin off of exact value, to avoid frequent toggling
+        root: null, rootMargin: `-${top + 10}px 0px 0px 0px`, threshold: 0,
       }).observe(sentinel);
 
       h.addEventListener('click', () => {
         if (location.hash !== `#${h.id}`) {
           history.pushState(null, '', `#${h.id}`);
         }
-        // scroll to, but use previous element for position anchor
+
+        // scroll to, but use the sticky-sentinel we seeded for position
         const r = h.previousElementSibling.getBoundingClientRect();
-        window.scrollBy({ top: r.top - top - 50, left: 0, behavior: 'smooth' });
+        window.scrollBy({ top: r.top - top - 150, left: 0, behavior: 'smooth' });
       });
     }
   });
